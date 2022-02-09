8c1dcde43bcf92e3b1bf2a3410bd27ef0f591a37
salahdin-ahmed <salah2112004@gmail.com>
2022-02-09 14:08:21 +0200

    Change `convert` function name and add comments

--- wpl/wpl.py
+++ wpl/wpl.py
@@ -1,8 +1,10 @@
 from re import sub
 
 
-def convert(template, code):
+# Convert the code using a template {{{1 #
+def convertCode(template: dict, code: str) -> str:
     for command in template:
         code = sub(command, template[command], code)
 
     return code
+# 1}}} #
