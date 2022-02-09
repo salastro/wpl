f10319fd267b3d901cd8fe8a6abfab75f37790ca
salahdin-ahmed <salah2112004@gmail.com>
2022-02-09 14:09:03 +0200

    wpl.py: add `findLanguage` function

--- wpl/wpl.py
+++ wpl/wpl.py
@@ -8,3 +8,12 @@ def convertCode(template: dict, code: str) -> str:
 
     return code
 # 1}}} #
+
+
+# Find the language from file header {{{1 #
+def findLanguage(code: str) -> str:
+    lang = code.splitlines()[0]
+    lang = lang.replace('# ', '')
+
+    return lang
+# 1}}} #
